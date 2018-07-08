a96ff28d166f8669dad3fea86307cfde96576b91
suusan2go <ksuzuki180@gmail.com>
2018-07-09 08:12:47 +0900

    fix action

--- src/main/kotlin/com/suusan2go/kotlinfillclass/intentions/FillClassIntention.kt
+++ src/main/kotlin/com/suusan2go/kotlinfillclass/intentions/FillClassIntention.kt
@@ -3,21 +3,23 @@ package com.suusan2go.kotlinfillclass.intentions
 import com.intellij.codeInsight.intention.IntentionAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
+import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import org.jetbrains.kotlin.psi.KtFile
 
 class FillClassIntention: IntentionAction {
     override fun getFamilyName(): String {
-        return getText()
+        return text
     }
 
     override fun getText(): String = "Fill class constructor"
 
-    override fun invoke(p0: Project, p1: Editor?, p2: PsiFile?) {
+    override fun invoke(project: Project, editor: Editor, psiFile: PsiFile) {
         return
     }
 
-    override fun isAvailable(p0: Project, p1: Editor?, p2: PsiFile?): Boolean {
-        return true
+    override fun isAvailable(project: Project, editor: Editor?, psiFile: PsiFile): Boolean {
+        return psiFile is KtFile
     }
 
     override fun startInWriteAction() = true
