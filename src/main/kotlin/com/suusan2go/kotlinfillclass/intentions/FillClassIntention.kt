7123a1e7f732a8d3783ccd8bc9b92798b2fbf5c6
suusan2go <ksuzuki180@gmail.com>
2018-07-13 18:31:06 +0900

    developing...

--- src/main/kotlin/com/suusan2go/kotlinfillclass/intentions/FillClassIntention.kt
+++ src/main/kotlin/com/suusan2go/kotlinfillclass/intentions/FillClassIntention.kt
@@ -5,11 +5,11 @@ import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
-import com.intellij.psi.PsiElement
-import com.intellij.psi.PsiFile
-import com.intellij.psi.PsiManager
-import com.intellij.psi.PsiReference
+import com.intellij.psi.*
+import com.intellij.psi.util.PsiTreeUtil
+import org.jetbrains.kotlin.asJava.KtLightClass
 import org.jetbrains.kotlin.asJava.toLightClassOrigin
+import org.jetbrains.kotlin.parsing.KotlinParser
 import org.jetbrains.kotlin.psi.*
 import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
 
@@ -23,12 +23,10 @@ class FillClassIntention: IntentionAction {
     override fun getText(): String = "Fill class constructor"
 
     override fun invoke(project: Project, editor: Editor, psiFile: PsiFile) {
-        val caretModel = editor.caretModel
-        val offset = caretModel.offset
         val ktFile = psiFile as KtFile
-        val psiElement = ktFile.findElementAt(offset)
+        val element = ktFile.findElementAt(editor.caretModel.offset)
 
-        psiElement!!.node.elementType
+        val clazz =  PsiTreeUtil.getParentOfType(element, KtLightClass::class.java, false)
         val file = PsiManager.getInstance(project).findFile(psiFile.virtualFile) as KtFile
         val factory = KtPsiFactory(project = project)
         val argument = factory.createStringTemplate("hogehoge")
