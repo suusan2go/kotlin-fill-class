75085a72337fe785385dcd715c5a6db5aac5c349
suusan2go <ksuzuki180@gmail.com>
2018-07-13 17:44:56 +0900

    developing...

--- src/main/kotlin/com/suusan2go/kotlinfillclass/intentions/FillClassIntention.kt
+++ src/main/kotlin/com/suusan2go/kotlinfillclass/intentions/FillClassIntention.kt
@@ -1,12 +1,18 @@
 package com.suusan2go.kotlinfillclass.intentions
 
 import com.intellij.codeInsight.intention.IntentionAction
+import com.intellij.openapi.command.WriteCommandAction
 import com.intellij.openapi.editor.Editor
+import com.intellij.openapi.fileEditor.FileEditorManager
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
-import org.jetbrains.kotlin.psi.KtElementImpl
-import org.jetbrains.kotlin.psi.KtFile
+import com.intellij.psi.PsiManager
+import com.intellij.psi.PsiReference
+import org.jetbrains.kotlin.asJava.toLightClassOrigin
+import org.jetbrains.kotlin.psi.*
+import org.jetbrains.kotlin.psi.psiUtil.getParentOfType
+
 
 class FillClassIntention: IntentionAction {
 
@@ -17,12 +23,21 @@ class FillClassIntention: IntentionAction {
     override fun getText(): String = "Fill class constructor"
 
     override fun invoke(project: Project, editor: Editor, psiFile: PsiFile) {
-        val file = psiFile as KtFile
-        file.add(KtElementImpl)
-        file.psiRoots.iterator().forEach {
-        }
+        val caretModel = editor.caretModel
+        val offset = caretModel.offset
+        val ktFile = psiFile as KtFile
+        val psiElement = ktFile.findElementAt(offset)
+
+        psiElement!!.node.elementType
+        val file = PsiManager.getInstance(project).findFile(psiFile.virtualFile) as KtFile
+        val factory = KtPsiFactory(project = project)
+        val argument = factory.createStringTemplate("hogehoge")
+
+        file!!.node.addChild(argument.node)
         return
     }
+
+
 
     override fun isAvailable(project: Project, editor: Editor?, psiFile: PsiFile): Boolean {
         return true
