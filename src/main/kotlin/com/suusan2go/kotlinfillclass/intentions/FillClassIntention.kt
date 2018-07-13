1e28a541807eb9b6ad61379bcd9cb3432c02cc80
suusan2go <ksuzuki180@gmail.com>
2018-07-13 13:57:56 +0900

    fix builds

--- src/main/kotlin/com/suusan2go/kotlinfillclass/intentions/FillClassIntention.kt
+++ src/main/kotlin/com/suusan2go/kotlinfillclass/intentions/FillClassIntention.kt
@@ -5,9 +5,11 @@ import com.intellij.openapi.editor.Editor
 import com.intellij.openapi.project.Project
 import com.intellij.psi.PsiElement
 import com.intellij.psi.PsiFile
+import org.jetbrains.kotlin.psi.KtElementImpl
 import org.jetbrains.kotlin.psi.KtFile
 
 class FillClassIntention: IntentionAction {
+
     override fun getFamilyName(): String {
         return text
     }
@@ -15,11 +17,15 @@ class FillClassIntention: IntentionAction {
     override fun getText(): String = "Fill class constructor"
 
     override fun invoke(project: Project, editor: Editor, psiFile: PsiFile) {
+        val file = psiFile as KtFile
+        file.add(KtElementImpl)
+        file.psiRoots.iterator().forEach {
+        }
         return
     }
 
     override fun isAvailable(project: Project, editor: Editor?, psiFile: PsiFile): Boolean {
-        return psiFile is KtFile
+        return true
     }
 
     override fun startInWriteAction() = true
